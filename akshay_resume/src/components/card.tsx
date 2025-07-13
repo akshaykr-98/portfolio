652d2da07d795c473c5f70f951c6f7e3c5496d4f
akshaykr-98 <akshaykr98012@gmail.com>
2025-07-14 00:29:42 +0530

    footer

--- akshay_resume/src/components/card.tsx
+++ akshay_resume/src/components/card.tsx
@@ -4,11 +4,18 @@
 export default function Card(data){  
   return(
      <div className='card' key={data.id}>
-        <h3>{data.title}</h3>
-        <img src={data.thumbnail} alt="React" height={100} width={100} />
-        {/* <Image src={reactImg} alt="React" height='10' width='100' /> */}
+      <div className="card-inner">
+        <div className="card-front">
+          <img src={data.thumbnail} alt="React"/>
+          {/* <Image src={reactImg} alt="React" height='10' width='100' /> */}
+         <h3><b>{data.title}</b></h3>
 
-        <p>{data.description}</p>
+        </div>
+        <div className="card-back">
+          <h3><b>{data.title}</b></h3>
+          <p>{data.description}</p>
+        </div>
+      </div>
       </div>
   )
 } 
--- akshay_resume/src/components/footer.tsx
+++ akshay_resume/src/components/footer.tsx
@@ -1,8 +1,20 @@
+import Image from "next/image";
+import Link from "next/link";
+import GitHub from '../../Asstes/images/GitHub_Logo.svg'
+import faceBook from '../../Asstes/images/facebook-color-svgrepo-com.svg'
+import Linkedin from '../../Asstes/images/linkedin-linked-in-svgrepo-com.svg'
+import phone from '../../Asstes/images/phone-calling-svgrepo-com.svg'
+import email from '../../Asstes/images/email-part-2-svgrepo-com.svg'
+
+
 export default function Contacts(){
     return(
         <div className="footer" id="footer">
-            <p>Hello World</p>
-            <a href="https://www.google.com/">Goooglleeee</a>
+            <p><Image src={email} alt="" height={200} width={20}/> <span><b>akkikr494@gmail.com</b></span></p>
+            <p><Image src={phone} alt="" height={200} width={20}/> <span><b>+91-9801295690</b></span></p>
+            <span><b><a href="https://github.com/akshaykr-98" target="_blank" rel="noopener noreferrer"><Image src={GitHub} alt="" height={200} width={20}/></a></b></span>
+            <span><b><a href="www.linkedin.com/in/akshay-kumar-9b303517a" target="_blank" rel="noopener noreferrer"><Image src={Linkedin} alt="" height={200} width={20}/></a></b></span>
+            <span><b><a href="https://www.facebook.com/mrakshaykr/" target="_blank" rel="noopener noreferrer"><Image src={faceBook} alt="" height={200} width={20}/></a></b></span>
         </div>
     )
 }
